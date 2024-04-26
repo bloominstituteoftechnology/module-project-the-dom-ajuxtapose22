1d3fd61ec35b9b363c3ad57c935f674e872a8d69
ajuxtapose22 <aaron.jardin@yahoo.com>
2024-04-26 11:19:53 -0700

    challenge 3 Passed

--- frontend/index.js
+++ frontend/index.js
@@ -8,17 +8,61 @@ function moduleProject1() {
   // 👉 TASK 1 - Add a "widget" class name to widgets so CSS kicks in
   //  ✨ add your code here
     const widgets = document.querySelectorAll("div");
-    console.log(widgets);
-    
-    widgets.forEach(widget => {
+      widgets.forEach(widget => {
       widget.classList.add("widget"); 
     })
-    console.log(widgets);
+    
   // 👉 TASK 2 - Build a "Quote of the Day" widget
   //  ✨ add your code here
+// Random Quote
+const getRandom = Math.floor(Math.random() * quotes.length);
+const randomQuote = quotes[getRandom];
+// New Element 
+const newDivQuote = document.createElement("div");
+newDivQuote.textContent = randomQuote.quote;
+const quoteOfTheDayDiv = document.querySelector("div.quoteoftheday");
+quoteOfTheDayDiv.appendChild(newDivQuote);
+//Author & Date
+const authorDate = document.createElement('div');
+const {author, date} = randomQuote;
+authorDate.textContent = `${author} in ${date || "an unknown date"}`;
+const authorDateDiv = document.querySelector("div.quoteoftheday");
+authorDateDiv.appendChild(authorDate);
 
-  // 👉 TASK 3 - Build a "Corporate Speak" widget
-  //  ✨ add your code here
+
+// 👉 TASK 3 - Build a "Corporate Speak" widget
+//  ✨ add your code here
+// Random Adverb1
+  const randomAdverb = Math.floor(Math.random() * adverbs.length);
+  const adverb1 = adverbs[randomAdverb];
+// Random Noun1
+  const randomNoun = Math.floor(Math.random() * nouns.length);
+  const noun1 = nouns[randomNoun];
+// Random Verb1
+  const randomVerb = Math.floor(Math.random() * verbs.length);
+  const verb1 = verbs[randomVerb];
+
+// Random Adverb2
+  const randomAdverb2 = Math.floor(Math.random() * adverbs.length);
+  const adverb2 = adverbs[randomAdverb2];
+// Random Noun2
+  const randomNoun2 = Math.floor(Math.random() * nouns.length);
+  const noun2 = nouns[randomNoun2];
+// Random Verb2
+  const randomVerb2 = Math.floor(Math.random() * verbs.length);
+  const verb2 = verbs[randomVerb2];
+
+   
+
+// new element
+const paragraph = document.createElement("p");
+const mumboJumbo = `We need to ${verb1} our ${noun1} ${adverb1} 
+in order to ${verb2} our ${noun2} ${adverb2}.`
+paragraph.textContent = mumboJumbo;
+
+const sentence = document.querySelector(".corporatespeak")
+sentence.appendChild(paragraph)
+
 
   // 👉 TASK 4 - Build a "Countdown" widget
   //  ✨ add your code here
