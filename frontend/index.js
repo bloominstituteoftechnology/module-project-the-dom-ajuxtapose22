785088e6ea0a4f8104d3b9d914fb581d124d01c6
ajuxtapose22 <aaron.jardin@yahoo.com>
2024-04-26 08:46:10 -0700

    challenge 1 passed

--- frontend/index.js
+++ frontend/index.js
@@ -7,7 +7,13 @@ function moduleProject1() {
 
   // 👉 TASK 1 - Add a "widget" class name to widgets so CSS kicks in
   //  ✨ add your code here
-
+    const widgets = document.querySelectorAll("div");
+    console.log(widgets);
+    
+    widgets.forEach(widget => {
+      widget.classList.add("widget"); 
+    })
+    console.log(widgets);
   // 👉 TASK 2 - Build a "Quote of the Day" widget
   //  ✨ add your code here
 
